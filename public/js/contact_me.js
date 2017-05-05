b7314f6dcbae1ead064ff3a9f8ff75f2aec8d8e0
Elliot Campbelton <e.b.campbelton@gmail.com>
2017-05-05 20:33:17 +0100

    d

--- public/js/contact_me.js
+++ public/js/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "/form",
+                url: "https://adsapp.eu/form",
                 type: "POST",
                 data: {
                     name: name,
